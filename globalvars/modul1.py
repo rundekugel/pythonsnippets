bec1c5878b12698e3f0c884b81780c28b579d5cc
beb <b>
2021-03-08 10:12:10 +0100

    more simple solution added

--- globalvars/modul1.py
+++ globalvars/modul1.py
@@ -2,32 +2,40 @@
 """globals variables demo"""
 
 import globalsContainer
+import globalsContainerSimple
 import modul2
 
 def f1():
   gc = globalsContainer
-  print("m1.f1:",gc.varA1, gc.myglobals.varC1)
+  print("m1.f1:",gc.varA1, gc.myglobals.varC1, globalsContainerSimple.testvar)
   print("m1.f1 change values")
   gc.varA1 = 111
   gc.myglobals.varC1 =112
+  globalsContainerSimple.testvar = 1.1
   
   
 def f2():
   gc = globalsContainer
-  print("m1.f2:",gc.varA1, gc.myglobals.varC1)
+  print("m1.f2:",gc.varA1, gc.myglobals.varC1, globalsContainerSimple.testvar)
   print("m1.f2 change values")
   gc.varA1 = 121
   gc.myglobals.varC1 =122
+  globalsContainerSimple.testvar = 1.2
 
 def main():
   print("Verify the address of the class myglobals, because it's used as static class, \
        all the modules access the same variables address.)")
   print("m1:", globalsContainer.myglobals.varCHallo, globalsContainer.myglobals, modul2.globalsContainer.myglobals )
-  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
+  gcInstance = globalsContainer.myglobals() # create an instance
+  globalsContainerSimple.testvar = "init"
+  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1, globalsContainerSimple.testvar)
+  gcInstance.varA1 = 0.1
+  gcInstance.varC1 = 0.2
+  print("m1.m value of instance: ", gcInstance.varC1)
   f1()
-  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
+  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1, globalsContainerSimple.testvar)
   f2()
-  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
+  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1, globalsContainerSimple.testvar)
   
   print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
   modul2.f1()
@@ -39,7 +47,8 @@ def main():
   globalsContainer.varA1, globalsContainer.myglobals.varC1 = 91,92
   print("m1.m change values")
   modul2.main()
-  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
+  print("m1.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1, globalsContainerSimple.testvar)
+  print("m1.m value of instance: ", gcInstance.varC1)
   
 main()
-input()
+
--- globalvars/modul2.py
+++ globalvars/modul2.py
@@ -2,26 +2,30 @@
 """globals variables demo"""
 
 import globalsContainer
+import globalsContainerSimple
 
 def f1():
   gc = globalsContainer
-  print("m2.f1:",gc.varA1, gc.myglobals.varC1)
+  print("m2.f1:",gc.varA1, gc.myglobals.varC1, globalsContainerSimple.testvar)
   print("m2.f1 change values")
   gc.varA1 = 711
   gc.myglobals.varC1 =712
+  globalsContainerSimple.testvar=2.1
   
   
 def f2():
   gc = globalsContainer
-  print("m2.f2:",gc.varA1, gc.myglobals.varC1,globalsContainer.myglobals)
+  print("m2.f2:",gc.varA1, gc.myglobals.varC1,globalsContainer.myglobals, globalsContainerSimple.testvar)
   print("m2.f2 change values")
   gc.varA1 = 721
   gc.myglobals.varC1 =722
+  globalsContainerSimple.f2="hi"
+  globalsContainerSimple.testvar=2.2
 
 def main():
-  print("m2.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
+  print("m2.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1, globalsContainerSimple.testvar)
   f1()
   print("m2.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1)
   f2()
-  print("m2.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1,globalsContainer.myglobals)
+  print("m2.m:",globalsContainer.varA1, globalsContainer.myglobals.varC1,globalsContainer.myglobals, globalsContainerSimple.testvar)
   
